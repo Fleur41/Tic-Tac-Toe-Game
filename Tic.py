e731ca381ae7e503cba70a1db5f59f09c1156663
Fleur41 <Muriithikings456@gmail.com>
2023-12-21 14:06:05 +0300

    create board

--- Tic.py
+++ Tic.py
@@ -2,3 +2,17 @@
 Tic-Tac-Toe Game
 -------------------------------------------------------------
 '''
+
+import random
+
+class TicTacToe:
+
+    def __init__(self) -> None:
+        self.board = []
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.apend('-')
+            self.board.append(row)
